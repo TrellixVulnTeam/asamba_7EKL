c835a74d232cc8d747c8ee627c2d0c4c43338bac
Ehsan Moravveji <e.moravveji@gmail.com>
2017-01-10 10:52:00 +0100

    finished write.write_model_parameters_to_ascii()

--- write.py
+++ write.py
@@ -17,12 +17,58 @@ def write_model_parameters_to_ascii(self_models, ascii_out):
   if n_models == 0:
     logger.error('write_model_parameters_to_ascii: the passed "models" object has no models inside')
     sys.exit(1)
+  list_models = sm.get_list_models()
 
-  avail_attrs = dir(sm)
-  exclude     = set(['__init__', '__doc__', '__module__'])
+  # open the file handle
+  if os.path.exists(ascii_out): os.unlink(ascii_out)
+  try:
+    handle    = open(ascii_out, 'a')
+  except:
+    logger.error('write_model_parameters_to_ascii: failed to open: "{0}"'.format(ascii_out))
+    sys.exit(1)
+
+  # filter the attributes
+  first_model = list_models[0]
+  avail_attrs = dir(first_model)
+  exclude     = set(['__init__', '__doc__', '__module__', 'filename', 'track'])
   avail_attrs = [attr for attr in avail_attrs if attr not in exclude]
-  avail_attrs = [attr for attr in avail_attrs if 'set' not in attr or 'get' not in attr]
-  print avail_attrs
+  avail_attrs = [attr for attr in avail_attrs if 'set' not in attr and 'get' not in attr]
+  
+  key_attrs   = ['M_ini', 'fov', 'Z', 'logD', 'evol', 'Xc', 'model_number']
+  # key_fmt     = [float, float, float, float, str, float, int]
+  other_attrs = [attr for attr in avail_attrs if attr not in key_attrs]
+
+  # collect the header for all columns
+  header      = '{0:>6s} {1:>5s} {2:>5s} {3:>5s} {4:>4s} {5:>6s} {6:>5s}'.format(
+                 'M_ini', 'fov', 'Z', 'logD', 'evol', 'Xc', 'num')
+  for attr in other_attrs:
+    header    += '{0:>12s} '.format(attr[:12])
+  header      += '\n'
+
+  # write the header
+  handle.write(header)
+
+  # collect the line info as lines
+  lines       = [header]
+
+  # iterate over models, and collect data into lines
+  for i, model in enumerate(list_models):
+    # first, the key attributes
+    line      = '{0:>06.3f} {1:>05.3f} {2:>05.3f} {3:>05.2f} {4:>4s} {5:>06.4f} {6:>05d} '.format(
+                 model.M_ini, model.fov, model.Z, model.logD, model.evol, model.Xc, model.model_number)
+    
+    # iterate over the rest of the attributes, and convert them to string
+    for k, attr in other_attrs: line += '{0:>12.6e3} '.format(getattr(model, attr))
+    line += '\n'
+
+    # append to the ascii file, and to the output list
+    handle.write(line)
+    lines.append(line)
+
+  logger.info('write_model_parameters_to_ascii: saved "{0}"'.format(ascii_out))
+  print ' - grid.write.write_model_parameters_to_ascii: saved "{0}"'.format(ascii_out)
+
+  return lines
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 def write_tracks_parameters_to_ascii(self_tracks, ascii_out):
